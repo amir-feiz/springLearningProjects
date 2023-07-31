371c6ab91e3b45c5801e5a5de30237f724780340
amir-feiz <103141846+amir-feiz@users.noreply.github.com>
2023-07-31 16:06:49 +0330

    send json list response-send Students list

--- RestApi/src/main/java/com/example/restapi/controller/StudentController.java
+++ RestApi/src/main/java/com/example/restapi/controller/StudentController.java
@@ -4,6 +4,9 @@ import com.example.restapi.model.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
 
@@ -15,6 +18,17 @@ public class StudentController {
                 "amir-uname",
                 "1234"
         );
+    }
+
+    @GetMapping("/get-students")
+    public List<Student> sendStudents(){
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("amir", "feiz","amir-uname","1"));
+        students.add(new Student("ali", "alavi","ali-uname","2"));
+        students.add(new Student("hossein", "hosseini","hossein-uname","3"));
+        students.add(new Student("mamad", "mamadi","mamad-uname","4"));
+
+        return students;
     }
 
 }
