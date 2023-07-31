b16e3f1ddc137b84fca2929c7c59193bd43d0f14
amir-feiz <103141846+amir-feiz@users.noreply.github.com>
2023-07-31 15:40:34 +0330

    send string response-simple rest api

---
+++ RestApi/src/main/java/com/example/restapi/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.example.restapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/helloString")
+    public String printHello(){
+        return "hello my friends";
+    }
+
+}
